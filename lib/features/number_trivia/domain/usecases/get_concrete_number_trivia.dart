f5a5fe43a0cc10ae4c92886da85773495a80571f
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-14 18:59:19 +0430

    generate GetConcretNumberTrivia class

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  GetConcreteNumberTrivia(this.repository);
+}
