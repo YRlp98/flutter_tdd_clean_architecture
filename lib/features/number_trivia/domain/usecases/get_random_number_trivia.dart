259226e3a483ec4ac02e5ecc1a2c96508da12015
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-14 21:30:45 +0430

    generate get_random_number_trivia class

--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -4,7 +4,7 @@ import 'package:flutter_tdd_clean_architecture/core/error/failures.dart';
 import 'package:flutter_tdd_clean_architecture/core/usecases/usecase.dart';
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/entites/number_trivia.dart';
 
-class getRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
+class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
   @override
   Future<Either<Failure, NumberTrivia>> call(NoParams params) {
     // TODO: implement call
--- test/features/number_trivia/domian/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domian/usecases/get_random_number_trivia_test.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/entites/number_trivia.dart';
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 
