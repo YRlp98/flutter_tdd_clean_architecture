d6bf9081da1caffb5536da75b865924180c60b7a
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-14 19:40:22 +0430

    implment GetConcreteNumberTrivia class

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,7 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_tdd_clean_architecture/core/error/failures.dart';
+import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/entites/number_trivia.dart';
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
 class GetConcreteNumberTrivia {
   final NumberTriviaRepository repository;
 
   GetConcreteNumberTrivia(this.repository);
+
+  Future<Either<Failure, NumberTrivia>> execute({
+    @required int number,
+  }) async {
+    return await repository.getConcreteNumberTrivia(number);
+  }
 }
