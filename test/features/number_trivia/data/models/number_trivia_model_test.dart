224903e332d3625f5195c39da3b5824a2c912a9a
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-15 21:04:02 +0430

    test toJson

--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -7,10 +7,10 @@ import 'package:flutter_test/flutter_test.dart';
 import '../../../../fixtures/fixture_reader.dart';
 
 void main() {
-  final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'test text');
+  final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'Test Text');
 
   test(
-    'should be of NumberTrivia entity',
+    'should be a subclass of NumberTrivia entity',
     () async {
       // assert
       expect(tNumberTriviaModel, isA<NumberTrivia>());
@@ -18,16 +18,43 @@ void main() {
   );
 
   group('fromJson', () {
-    test('should return a valid model when the JSON number is as integer',
-        () async {
-      // arrange
-      final Map<String, dynamic> jsonMap = json.decode(fixture('trivia.json'));
+    test(
+      'should return a valid model when the JSON number is an integer',
+      () async {
+        // arrange
+        final Map<String, dynamic> jsonMap =
+            json.decode(fixture('trivia.json'));
+        // act
+        final result = NumberTriviaModel.fromJson(jsonMap);
+        // assert
+        expect(result, tNumberTriviaModel);
+      },
+    );
 
+    test(
+      'should return a valid model when the JSON number is regarded an double',
+      () async {
+        // arrange
+        final Map<String, dynamic> jsonMap =
+            json.decode(fixture('trivia_double.json'));
+        // act
+        final result = NumberTriviaModel.fromJson(jsonMap);
+        // assert
+        expect(result, tNumberTriviaModel);
+      },
+    );
+  });
+
+  group('toJson', () {
+    test('Should return a JSON map containg the proper data', () async {
       // act
-      final result = NumberTriviaModel.fromJson(jsonMap);
-
+      final result = tNumberTriviaModel.toJson();
       // assert
-      expect(result, tNumberTriviaModel);
+      final expectedMap = {
+        "text": "Test Text",
+        "number": 1,
+      };
+      expect(result, expectedMap);
     });
   });
 }
