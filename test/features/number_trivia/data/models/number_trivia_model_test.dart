5af9d91cb95fbb9966ef44d6279ee421bb6ba134
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-15 20:22:45 +0430

    test json number type

--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/entites/number_trivia.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
 
 void main() {
   final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'test text');
@@ -12,4 +16,18 @@ void main() {
       expect(tNumberTriviaModel, isA<NumberTrivia>());
     },
   );
+
+  group('fromJson', () {
+    test('should return a valid model when the JSON number is as integer',
+        () async {
+      // arrange
+      final Map<String, dynamic> jsonMap = json.decode(fixture('trivia.json'));
+
+      // act
+      final result = NumberTriviaModel.fromJson(jsonMap);
+
+      // assert
+      expect(result, tNumberTriviaModel);
+    });
+  });
 }
