af0241a17e0fb4dde48b6004ddb083c32c47cede
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-16 18:14:32 +0430

    write checking device online test

--- test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
+++ test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_tdd_clean_architecture/core/platform/network_info.dart';
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/data/datasources/numbe_trivia_remote_data_source.dart';
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:flutter_tdd_clean_architecture/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/entites/number_trivia.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -27,5 +29,21 @@ void main() {
       localDataSource: mockLocalDataSource,
       networkInfo: mockNetworkInfo,
     );
+  });
+
+  group('getConcretedNumberTrivia', () {
+    final tNumber = 1;
+    final tNumberTriviaModel =
+        NumberTriviaModel(text: 'Test Trivi', number: tNumber);
+    final NumberTrivia tNumberTrivia = tNumberTriviaModel;
+
+    test('Should check if the device is online', () async {
+      // arrange
+      when(mockNetworkInfo.isConnected).thenAnswer((_) async => true); //
+      // act
+      repository.getConcreteNumberTrivia(tNumber);
+      // assert
+      verify(mockNetworkInfo.isConnected);
+    });
   });
 }
