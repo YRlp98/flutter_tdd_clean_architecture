f58e4390c09bbd052dc2f2be0e817d719e2b5c0c
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-17 20:21:44 +0430

    implement CacheException test

--- lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,17 +1,19 @@
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
+import 'package:flutter_tdd_clean_architecture/core/error/exceptions.dart';
+import 'package:meta/meta.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/number_trivia_model.dart';
 
 abstract class NumberTriviaLocalDataSource {
-  /// Gets the chached [NumberTriviaModel] which was gotten the last time
+  /// Gets the cached [NumberTriviaModel] which was gotten the last time
   /// the user had an internet connection.
   ///
-  /// Throws [CacheException] if no chached data is present.
+  /// Throws [CacheException] if no cached data is present.
   Future<NumberTriviaModel> getLastNumberTrivia();
-  Future<void> cacheNumberTrivia(NumberTriviaModel);
+
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
 }
 
 const CACHED_NUMBER_TRIVIA = 'CACHED_NUMBER_TRIVIA';
@@ -24,7 +26,11 @@ class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
   @override
   Future<NumberTriviaModel> getLastNumberTrivia() {
     final jsonString = sharedPreferences.getString(CACHED_NUMBER_TRIVIA);
-    return Future.value(NumberTriviaModel.fromJson(json.decode(jsonString)));
+    if (jsonString != null) {
+      return Future.value(NumberTriviaModel.fromJson(json.decode(jsonString)));
+    } else {
+      throw CacheException();
+    }
   }
 
   @override
--- test/features/number_trivia/data/datasources/number_trivia_local_data_source_test.dart
+++ test/features/number_trivia/data/datasources/number_trivia_local_data_source_test.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
 
-import 'package:flutter/widgets.dart';
 import 'package:flutter_tdd_clean_architecture/core/error/exceptions.dart';
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/data/models/number_trivia_model.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:matcher/matcher.dart';
 
 import '../../../../fixtures/fixture_reader.dart';
 
@@ -19,19 +19,20 @@ void main() {
   setUp(() {
     mockSharedPreferences = MockSharedPreferences();
     dataSource = NumberTriviaLocalDataSourceImpl(
-        sharedPreferences: mockSharedPreferences);
+      sharedPreferences: mockSharedPreferences,
+    );
   });
 
   group('getLastNumberTrivia', () {
     final tNumberTriviaModel =
-        NumberTriviaModel.fromJson(json.decode(fixture('trivia_cache.json')));
+        NumberTriviaModel.fromJson(json.decode(fixture('trivia_cached.json')));
 
     test(
-      'Should return NumberTrivia from SharedPreferences when there is one in the cache',
+      'should return NumberTrivia from SharedPreferences when there is one in the cache',
       () async {
         // arrange
         when(mockSharedPreferences.getString(any))
-            .thenReturn(fixture('trivia_cache.json'));
+            .thenReturn(fixture('trivia_cached.json'));
         // act
         final result = await dataSource.getLastNumberTrivia();
         // assert
@@ -41,14 +42,14 @@ void main() {
     );
 
     test(
-      'Should hrow a CacheExeption when there is not a cached value',
+      'should throw a CacheExeption when there is not a cached value',
       () async {
         // arrange
         when(mockSharedPreferences.getString(any)).thenReturn(null);
         // act
-        final call = dataSource.getLastNumberTrivia();
+        final call = dataSource.getLastNumberTrivia;
         // assert
-        expect(() => call, throwsA(TypeMatcher<CacheException>()));
+        expect(() => call(), throwsA(TypeMatcher<CacheException>()));
       },
     );
   });
