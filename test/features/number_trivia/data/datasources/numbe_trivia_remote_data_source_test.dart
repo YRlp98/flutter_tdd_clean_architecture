76a140fb78f0fb0a8ffd18d84ec7bc235cea6c2e
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-18 17:32:06 +0430

    write test for get request

--- test/features/number_trivia/data/datasources/numbe_trivia_remote_data_source_test.dart
+++ test/features/number_trivia/data/datasources/numbe_trivia_remote_data_source_test.dart
@@ -5,6 +5,8 @@ import 'package:mockito/mockito.dart';
 import 'package:matcher/matcher.dart';
 import 'package:http/http.dart' as http;
 
+import '../../../../fixtures/fixture_reader.dart';
+
 class MockHttpClient extends Mock implements http.Client {}
 
 void main() {
@@ -14,5 +16,21 @@ void main() {
   setUp(() {
     mockHttpClient = MockHttpClient();
     dataSource = NumberTriviaRemoteDataSourceImpl(client: mockHttpClient);
+  });
+
+  group('getConcreteNumberTrivia', () {
+    final tNumber = 1;
+
+    test('''should perform a GET requst on a URL with number
+        being the endpoint and with applcaition/json header''', () {
+      // arrange
+      when(mockHttpClient.get(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => http.Response(fixture('trivia.json'), 200));
+      // act
+      dataSource.getConcreteNumberTrivia(tNumber);
+      // asert
+      verify(mockHttpClient.get('http://numbersapi.com/$tNumber',
+          headers: {'Content-Type': 'application/json'}));
+    });
   });
 }
