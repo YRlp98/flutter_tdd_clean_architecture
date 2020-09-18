f5e3b0786ab1ea4b245ccf57160094e0171ed8ed
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-18 18:31:34 +0430

    fix imports

--- lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 
-import 'package:flutter_tdd_clean_architecture/core/error/exceptions.dart';
 import 'package:meta/meta.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../../../core/error/exceptions.dart';
 import '../models/number_trivia_model.dart';
 
 abstract class NumberTriviaLocalDataSource {
