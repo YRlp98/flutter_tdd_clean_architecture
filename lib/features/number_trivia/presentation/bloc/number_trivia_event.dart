741ae30d0cadf7cf71f19181f17145f100797bfe
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-20 19:52:02 +0430

    put numberString to the super constructor

--- lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
@@ -12,7 +12,7 @@ abstract class NumberTriviaEvent extends Equatable {
 class GetTriviaForConcreteNumber extends NumberTriviaEvent {
   final String numberString;
 
-  GetTriviaForConcreteNumber(this.numberString);
+  GetTriviaForConcreteNumber(this.numberString) : super([numberString]);
 }
 
 class GetTriviaForRandomNumber extends NumberTriviaEvent {}
